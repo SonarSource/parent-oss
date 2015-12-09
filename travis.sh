98bad41fa49445d5231c00056d245a64f31b4486
Simon Brandhof <simon.brandhof@sonarsource.com>
2015-12-09 16:51:11 +0100

    Fix travis.sh

--- travis.sh
+++ travis.sh
@@ -7,8 +7,9 @@ function installTravisTools {
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
+installTravisTools
 
 # required as long as version 1.1-SNAPSHOT is being used
 build_snapshot "SonarSource/license-headers"
 
-mvn package -B -V -e
+mvn verify -B -V -e
