c2af8a5e24dbc5f5a175fdf25717c8a5b361b2d6
David Rautureau <david.rautureau@sonarsource.com>
2016-05-09 10:43:58 +0200

    Enable promotion

--- travis.sh
+++ travis.sh
@@ -2,4 +2,26 @@
 
 set -euo pipefail
 
-mvn verify -B -V -e
+function installTravisTools {
+  mkdir ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v28 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+
+installTravisTools
+
+# get current version from pom
+CURRENT_VERSION=`maven_expression "project.version"`
+
+if [[ $CURRENT_VERSION =~ "-SNAPSHOT" ]]; then
+  echo "======= Found SNAPSHOT version ======="
+  # Do not deploy a SNAPSHOT version but the release version related to this build
+  set_maven_build_version $TRAVIS_BUILD_NUMBER
+else
+  echo "======= Found RELEASE version ======="
+fi
+
+export MAVEN_OPTS="-Xmx1536m -Xms128m"
+mvn deploy \
+    -Pdeploy-sonarsource,release \
+    -B -e -V
