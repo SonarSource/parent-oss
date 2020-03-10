e4f0779031a7b1fe79bc816eb6ce9bb0efb9fd4d
Tom <thomas.verin@sonarsource.com>
2020-03-10 16:07:11 +0100

    update travis utils to v58

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/b0ad621962900d63f670989f377228d076929a59 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v58 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
@@ -14,4 +14,4 @@ source setup_promote_environment
 
 source build_and_promote_and_notify
 
-source do_parent_pom_release "sonarsource-public-releases"
+source release "sonarsource-public-releases"
